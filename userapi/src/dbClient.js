cd7d22b88bb48e190df982656ae2be02a30f3f49
mori.huang <mori.official.1995@gmail.com>
2023-05-07 16:22:25 +0200

    feat: kubernetes initialize

--- userapi/src/dbClient.js
+++ userapi/src/dbClient.js
@@ -5,8 +5,10 @@ const config = configure()
 var db = redis.createClient({
   host: config.redis.host,
   port: config.redis.port,
+  username: config.redis.username,
+  password: config.redis.password,
   retry_strategy: () => {
-    return new Error("Retry time exhausted")
+    return new Error("Retry time exhausted" + ":" + config.redis.host + "-" + config.redis.port)
   }
 })
 
